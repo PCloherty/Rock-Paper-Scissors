8cb8f7c20be5cdaea2294426c1a238352cd757d1
Paraic Cloherty <p.cloherty@hotmail.co.uk>
2019-07-30 16:56:39 +0100

    Updates

---
+++ precache-manifest.c75fbee39a2f47218be6c761f7a94e11.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26f3d53459a849d021a57e9acd06373b",
+    "url": "/rock-paper-scissors/index.html"
+  },
+  {
+    "revision": "db27675ff2fa71a9554d",
+    "url": "/rock-paper-scissors/static/css/main.300a2a3b.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/rock-paper-scissors/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "db27675ff2fa71a9554d",
+    "url": "/rock-paper-scissors/static/js/main.abeff98e.chunk.js"
+  },
+  {
+    "revision": "cf8ffcfd822dc753bb49",
+    "url": "/rock-paper-scissors/static/js/runtime~main.c4e15f11.js"
+  }
+]);
